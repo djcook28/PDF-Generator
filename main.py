97117fcda296224ced981b1263887cdc29e6bab3
dcook <daniel.j.cook.28@gmail.com>
2025-03-08 19:53:17 -0600

    simplified add pages using for loop and range instead of while

--- main.py
+++ main.py
@@ -7,13 +7,11 @@ pdf.set_text_color(100,100,100)
 
 topics = pd.read_csv('topics.csv')
 
-for index, topic in topics.iterrows():
+for index, row in topics.iterrows():
     pdf.add_page()
-    pdf.cell(w=0, h=12, txt=topic['Topic'], ln=1)
+    pdf.cell(w=0, h=12, txt=row['Topic'], ln=1)
     pdf.line(10,20,200,20)
-    i = 1
-    while i <= int(topic['Pages'])-1:
+    for i in range(row['Pages'] - 1):
         pdf.add_page()
-        i = i+1
 
 pdf.output("output.pdf")
